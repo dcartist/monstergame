55ff1a3ba72ab9cd56ce7ded8e8a80e220aa3e20
DCartist <pbannerman13@gmail.com>
2021-01-31 19:36:36 -0500

    adjusting app

--- src/App.js
+++ src/App.js
@@ -8,19 +8,46 @@ import { Route, Link, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   const [UserweaponSelect, setUserWeapon] = useState(0);
+  const [fightNumber, setFightNumber] = useState(0);
   const [UserWarrior, setUserWarrior] = useState();
   const [Results, setResults] = useState("NO FIGHT YET");
   
   function Fight(){
-    let RandomWeapon = Math.floor(Math.random() * 3);
-    if (RandomWeapon == UserweaponSelect){
-      setResults("Equal")
-    }  else if (RandomWeapon < UserweaponSelect){
-      setResults(`${UserWarrior} wins!`)
-    } 
-    else {
-      setResults(`${UserWarrior} lost!`)
+    let RandomWeapon = Math.floor(Math.random() * 3)+1;
+    setFightNumber(RandomWeapon)
+    switch(UserweaponSelect){
+      case RandomWeapon:
+        setResults("Equal")
+        break;
+      case 3:
+        if (RandomWeapon == 1){
+            setResults(`${UserWarrior} lost!`)
+          } else {
+            setResults(`${UserWarrior} won!`)
+          }
+        break;
+      case 1:
+        if (RandomWeapon == 3){
+            setResults(`${UserWarrior} Wins!`)
+          } else {
+            setResults(`${UserWarrior} lost!`)
+          }
+        break;
+      default:
+        if (RandomWeapon == 3){
+          setResults(`${UserWarrior} lost!`)
+        } else {
+          setResults(`${UserWarrior} win!`)
+        }
     }
+    // if (RandomWeapon == UserweaponSelect){
+    //   setResults("Equal")
+    // }  else if (RandomWeapon < UserweaponSelect){
+    //   setResults(`${UserWarrior} wins!`)
+    // } 
+    // else {
+    //   setResults(`${UserWarrior} lost!`)
+    // }
   }
   return (
     <div className="full-width">
@@ -33,6 +60,7 @@ function App() {
     <h1>FIGHT!</h1>
     <p>  <button onClick={Fight}> FIGHT</button> </p>
    <h1>{Results}</h1>
+   {fightNumber}
     
 
 
