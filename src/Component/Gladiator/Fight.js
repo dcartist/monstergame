59d21cdf1b252606271d3dde1a810a06a3e8fa01
dcartist <pbannerman13@gmail.com>
2021-02-03 23:11:55 -0500

    updating fight section

--- src/Component/Gladiator/Fight.js
+++ src/Component/Gladiator/Fight.js
@@ -7,10 +7,10 @@ function Fight(props) {
     <div>
       <div className="flex flex-row justify-center items-center">
      
-      <div className="slideLeft"><img src={process.env.PUBLIC_URL + `/weapons/${props.UserweaponSelect}.png`} alt={`Paper`} className="w-9/12"/> </div>
+      {/* <div className="slideLeft"><img src={process.env.PUBLIC_URL + `/weapons/${props.UserweaponSelect}.png`} alt={`Paper`} className="w-9/12"/> </div> */}
       <div className="slideLeft"><img src={process.env.PUBLIC_URL + `/monsters/${props.UserWarrior}.png`} alt={`Paper`} className="w-9/12"/> </div>
       <div><h1 className="text-7xl p-14">VS.</h1></div>
-      <div className="slideRight"><img src={process.env.PUBLIC_URL + `/weapons/${props.CompWeapon}.png`} alt={`weapon`} className="w-9/12"/> </div>
+      {/* <div className="slideRight"><img src={process.env.PUBLIC_URL + `/weapons/${props.CompWeapon}.png`} alt={`weapon`} className="w-9/12"/> </div> */}
       <div className="slideRight"><img src={process.env.PUBLIC_URL + `/monsters/${props.CompWarrior}.png`} alt={`Paper`} className="w-9/12"/> </div>
       </div>
        <p>  <Link to="/results"><button onClick={props.fight} className="p-3 border-black border"> FIGHT</button></Link> </p> 
