f332c0988ad4cf1a19c34a455fd6b90419053d69
DCartist <pbannerman13@gmail.com>
2021-02-04 17:36:06 -0500

    adding konami comoponent

---
+++ src/Component/Konami/Konami.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Konami extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
