aabe8bfe4c7ea32be25566b47b9cbbb8f177a5d5
DCartist <pbannerman13@gmail.com>
2021-02-06 13:08:10 -0500

    updating animation

--- src/Component/Gladiator/Results.js
+++ src/Component/Gladiator/Results.js
@@ -6,7 +6,18 @@ import Fade from 'react-reveal/Fade';
 
 function Results (props) {
     const { width, height } = useWindowSize()
-    const [showResults, setshowResults] = useState(<Fade  big top cascade><div><h2 className="fadeIn expandOpen text-5xl p-3">Rock</h2><h2 className="fadeIn expandOpen text-5xl p-3">Paper</h2><h2 className="fadeIn expandOpen text-5xl p-3">Scissors</h2><h2 className="fadeIn expandOpen text-5xl p-3">Shoot!</h2></div></Fade>)
+    const [showResults, setshowResults] = useState(<Fade big top cascade>
+        <div className="flex justify-center flex-col flex-wrap">
+        <h2 className="fadeIn text-5xl p-3 m-auto">Rock</h2>
+        <hr style={{ color: 'red', backgroundColor: 'red', height: 4, width: 150}} className="slideDown m-auto"/>
+        <h2 className="fadeIn expandOpen text-5xl p-3 m-auto">Paper</h2>
+        <hr style={{ color: 'red', backgroundColor: 'red', height: 4, width: 150}} className="slideDown m-auto"/>
+    <h2 className="fadeIn  text-5xl p-3 m-auto">Scissors</h2>
+        <hr style={{ color: 'red', backgroundColor: 'red', height: 4, width: 150}} className="slideDown m-auto"/>
+    <h2 className="fadeIn text-8xl font-black p-3 m-auto">Shoot!</h2>
+
+    </div>
+    </Fade>)
 
     useEffect(() => {
         console.log('I hope this works');
@@ -15,7 +26,7 @@ function Results (props) {
 
                 {props.Winner === "Won!!!!"? <Confetti width={width} height={height}/> : <div></div>}
                 
-            <h1 className="text-7xl text-red-600 font-black animate expandOpen mt-10"> You {props.Winner} </h1>
+            <h1 className="text-7xl text-red-500 font-black animate expandOpen mt-10"> You {props.Winner} </h1>
             <p className="text-lg">Score: {props.Score} || Fights: {props.Fighttimes}</p>
            <div className="grid-cols-2 grid">
            <div>
@@ -35,7 +46,7 @@ function Results (props) {
             
             <p>  <Link to="/gladiator"><button onClick={props.reset} className="p-3 border-black border">  Fight Again! </button></Link></p>
         </div>);
-        }, 2200)
+        }, 2400)
         
       }, []);
     return (
