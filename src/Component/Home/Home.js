6a524895098051787968f6a2b8c188aca847626e
DCartist <pbannerman13@gmail.com>
2021-02-06 04:47:57 -0500

    updated home section

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,11 +4,11 @@ class Home extends Component {
     render() {
         return (
             <div className="flex flex-col h-96 items-center justify-center">
-                <h1 className="text-7xl font-thin">Monsters Arena</h1>
-                <div className="flex w-full">  <img src={process.env.PUBLIC_URL + `/monsters/1.svg`} className="w-3/12"/> 
-   <img src={process.env.PUBLIC_URL + `/monsters/2.svg`} className="w-3/12"/> 
-   <img src={process.env.PUBLIC_URL + `/monsters/4.svg`} className="w-3/12"/> 
-   <img src={process.env.PUBLIC_URL + `/monsters/3.svg`} className="w-3/12"/> </div>
+                <h1 className="text-7xl font-thin fadeIn">Monsters Arena</h1>
+                <div className="flex w-full">  <img src={process.env.PUBLIC_URL + `/monsters/1.svg`} className="w-3/12 hatch"/> 
+   <img src={process.env.PUBLIC_URL + `/monsters/2.svg`} className="w-3/12 hatch"/> 
+   <img src={process.env.PUBLIC_URL + `/monsters/4.svg`} className="w-3/12 hatch"/> 
+   <img src={process.env.PUBLIC_URL + `/monsters/3.svg`} className="w-3/12 hatch"/> </div>
             </div>
         );
     }
